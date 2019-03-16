da1ade6a6db1298092df8deaade2aad7e55f8cd3
amir <kmuc72@gmail.com>
2019-03-16 18:37:34 +0100

    search component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Layout, Page, Card } from '@shopify/polaris'
+import MultiAutocomplete from './MultiAutocomplete'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      first: '',
+      last: '',
+      email: '',
+      checkboxes: [],
+      connected: false,
+    }
+  }
+
   render() {
+    const breadcrumbs = [{ content: 'Sample apps' }, { content: 'Return to dashboard' }]
+    // const primaryAction = { content: 'New product' }
+    // const secondaryActions = [{ content: 'Import', icon: 'import' }]
+
+    const choiceListItems = [
+      { label: 'I accept the Terms of Service', value: 'false' },
+      { label: 'I consent to receiving emails', value: 'false2' },
+    ]
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Page
+        title="Create Mystery Box"
+        breadcrumbs={breadcrumbs}
+        // primaryAction={primaryAction}
+        // secondaryActions={secondaryActions}
+      >
+        <Layout>
+          <Layout.Section>
+            <Card title="Online store dashboard" sectioned>
+              <p>View a summary of your online store’s performance.</p>
+              <MultiAutocomplete />
+            </Card>
+          </Layout.Section>
+        </Layout>
+      </Page>
     )
   }
 }
---
+++ src/MultiAutocomplete.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import { Autocomplete, TextContainer, Stack, Tag } from '@shopify/polaris'
+
+class MultiAutocomplete extends Component {
+  options = [
+    { value: 'rustic', label: 'Rustic' },
+    { value: 'antique', label: 'Antique' },
+    { value: 'vinyl', label: 'Vinyl' },
+    { value: 'vintage', label: 'Vintage' },
+    { value: 'refurbished', label: 'Refurbished' },
+  ]
+
+  state = {
+    selected: [],
+    inputText: '',
+    options: this.options,
+  }
+  render() {
+    const textField = (
+      <Autocomplete.TextField
+        onChange={this.updateText}
+        label=""
+        value={this.state.inputText}
+        placeholder="Vintage, cotton, summer"
+      />
+    )
+    return (
+      <div style={{ height: '325px' }}>
+        <br />
+        <Autocomplete
+          allowMultiple
+          options={this.state.options}
+          selected={this.state.selected}
+          textField={textField}
+          onSelect={this.updateSelection}
+          listTitle="Suggested Tags"
+        />
+        <br />
+        <TextContainer>
+          <Stack>{this.renderTags()}</Stack>
+        </TextContainer>
+      </div>
+    )
+  }
+
+  updateText = (newValue) => {
+    this.setState({ inputText: newValue })
+    this.filterAndUpdateOptions(newValue)
+  }
+
+  removeTag = (tag) => {
+    let newSelected = this.state.selected
+    newSelected.splice(newSelected.indexOf(tag), 1)
+    this.setState({ selected: newSelected })
+  }
+
+  renderTags = () => {
+    return this.state.selected.map((option) => {
+      let tagLabel = ''
+      tagLabel = option.replace('_', ' ')
+      tagLabel = titleCase(tagLabel)
+      return (
+        <Tag key={'option' + option} onRemove={() => this.removeTag(option)}>
+          {tagLabel}
+        </Tag>
+      )
+    })
+  }
+
+  filterAndUpdateOptions = (inputString) => {
+    if (inputString === '') {
+      this.setState({
+        options: this.options,
+      })
+      return
+    }
+
+    const filterRegex = new RegExp(inputString, 'i')
+    const resultOptions = this.options.filter((option) => option.label.match(filterRegex))
+    let endIndex = resultOptions.length - 1
+    if (resultOptions.length === 0) {
+      endIndex = 0
+    }
+    this.setState({
+      options: resultOptions,
+    })
+  }
+  updateSelection = (selected) => this.setState({ selected })
+}
+export default MultiAutocomplete
+
+function titleCase(string) {
+  string = string
+    .toLowerCase()
+    .split(' ')
+    .map(function(word) {
+      return word.replace(word[0], word[0].toUpperCase())
+    })
+  return string.join(' ')
+}
